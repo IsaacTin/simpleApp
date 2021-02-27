f960e85cb7a0dfb02a7c3967bb02549e14948f8b
IsaacTin <tin.isaac@yahoo.com.sg>
2021-02-27 20:48:05 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Dogs from './data/dogs.json';
+import Cats from './data/cats.json';
+import Squirrels from './data/squirrels.json';
+import Rabbits from './data/rabbits.json';
 
 function App() {
+  const[state, setState] = useState("Dogs");
+  const[list, setList] = useState(Dogs.dogs);
+
+  const changeState = (newState, newList) => {
+    setState(newState);
+    setList(newList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="buttons-bar">
+          <button className="button" onClick={() => changeState("Dogs", Dogs.dogs)}> Dogs </button>
+          <button className="button" onClick={() => changeState("Cats", Cats.cats)}> Cats </button>
+          <button className="button" onClick={() => changeState("Squirrels", Squirrels.squirrels)}> Squirrels </button>
+          <button className="button" onClick={() => changeState("Rabbits", Rabbits.rabbits)}> Rabbits </button>
+        </div>
+        <h1>{state}</h1>
+        <div className="container">
+          {list.map(({id, src}) => <img alt="sorry" className="photos" key = {id} src = {require(`${src}`).default}/>)}
+        </div>
     </div>
   );
 }
